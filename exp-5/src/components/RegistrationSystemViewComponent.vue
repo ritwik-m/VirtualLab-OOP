5088cdb28b367eeb62a1182ef94c0a3a3ed48458
nitinravi <nitinravi18@gmail.com>
2022-12-24 16:26:05 +0530

    half validation done

---
+++ exp-5/src/components/RegistrationSystemViewComponent.vue
@@ -0,0 +1,406 @@
+<script>
+export default {
+
+  methods: {
+    validate_one() {
+      if (this.one == "Customer[]" && this.two == "Customer[3]") {
+        this.button_value = this.button_text_right;
+        btn2.style.backgroundColor = "green";
+        this.display[0] = "none";
+        this.display[1] = "inherit";
+      }
+
+      else {
+        this.button_value = this.button_text;
+        btn1.style.backgroundColor = "red";
+      }
+    },
+
+    validate_two() {
+      if (this.three == "new" && this.four == "ABC" && this.five == "abc@gmail.com") {
+        this.button_value = this.button_text_right;
+        btn3.style.backgroundColor = "green";
+        this.display[1] = "none";
+        this.display[2] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn2.style.backgroundColor = "red";
+      }
+    },
+
+    validate_three() {
+      if (this.six == "setCity" && this.seven == "Coimbatore") {
+        this.button_value = this.button_text_right;
+        btn4.style.backgroundColor = "green";
+        this.display[2] = "none";
+        this.display[3] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn3.style.backgroundColor = "red";
+      }
+    },
+    validate_four() {
+      if (this.seven == "String" && this.eight == "email") {
+        this.button_value = this.button_text_right;
+        btn5.style.backgroundColor = "green";
+        this.display[3] = "none";
+        this.display[4] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn4.style.backgroundColor = "red";
+      }
+    },
+    validate_five() {
+      if (this.nine == "long" && this.ten == "phone") {
+        this.button_value = this.button_text_right;
+        btn6.style.backgroundColor = "green";
+        this.display[4] = "none";
+        this.display[5] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn5.style.backgroundColor = "red";
+      }
+    },
+    validate_six() {
+      if (this.eleven == "String" && this.twelve == "city") {
+        this.button_value = this.button_text_right;
+        btn7.style.backgroundColor = "green";
+        this.display[5] = "none";
+        this.display[6] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn6.style.backgroundColor = "red";
+      }
+    },
+    validate_seven() {
+      if (this.thirteen == "static" && this.fourteen == "int" && this.fifteen == "customerCount=0") {
+        this.button_value = this.button_text_right;
+        btn8.style.backgroundColor = "green";
+        this.display[6] = "none";
+        this.display[7] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn7.style.backgroundColor = "red";
+      }
+    },
+    validate_eight() {
+      if (this.sixteen == "customerCount++" && this.seventeen == "this") {
+        this.button_value = this.button_text_right;
+        btn9.style.backgroundColor = "green";
+        this.display[7] = "none";
+        this.display[8] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn8.style.backgroundColor = "red";
+      }
+    },
+    validate_nine() {
+      if (this.eighteen == "String custName" && this.nineteen == "String email" && this.twenty == "this" && this.t1 == "this") {
+        this.button_value = this.button_text_right;
+        btn10.style.backgroundColor = "green";
+        this.display[8] = "none";
+        this.display[9] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn9.style.backgroundColor = "red";
+      }
+    },
+    validate_ten() {
+      if (this.t2 == "String custName" && this.t3 == "long phone" && this.t4 == "this" && this.t5 == `setPhone(phone)`) {
+        this.button_value = this.button_text_right;
+        btn11.style.backgroundColor = "green";
+        this.display[9] = "none";
+        this.display[10] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn10.style.backgroundColor = "red";
+      }
+    },
+
+    validate_eleven() {
+      if (this.t6 == "getcustName" && this.t7 == "return custName") {
+        this.button_value = this.button_text_right;
+        btn12.style.backgroundColor = "green";
+        this.display[10] = "none";
+        this.display[11] = "inherit";
+      }
+      else {
+        this.button_value = this.button_text;
+        btn11.style.backgroundColor = "red";
+      }
+    },
+
+    validate_twelve() {
+      if (this.t8 == "void" && this.t9 == "setPhone" && this.t10 == "long phone" && this.t11 == "if" && this.t12 == "this.phone" && this.t13 == "else" && this.t14 == "this.phone") {
+        this.button_value = this.button_text_right;
+        btn12.style.backgroundColor = "green";
+        this.display[11] = "none";
+        this.display[12] = "inherit";
+        this.cue = `Congratulations you have successfully completed learning to implement classes. The final code for the class is given below.`;
+      }
+      else {
+        this.button_value = this.button_text;
+        btn12.style.backgroundColor = "red";
+      }
+    },
+  },
+  data() {
+    return {
+      id: 'container',
+      class: 'wrapper',
+      button_value: 'Next',
+      button_text: "Incorrect. Try again",
+      button_text_right: "Correct. Next",
+      correct: false,
+      display: ["inherit", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none", "none",]
+    }
+  }
+
+}
+
+</script>
+<template>
+    <main>
+    <div class="row">
+            
+            <div class="column">
+              <h1 style="padding: 25px;">Enter your code here .</h1>
+      
+              <div class="codebox" id="codebox" :style="{ display: display[0] }">
+                <pre>
+import java.util.Scanner;
+import java.util.Date;
+
+public class TestDriver {
+
+    public static void main(String[] args) {</pre>&emsp;
+                <input spellcheck="false" maxlength="10" style="width: 100px;" v-model='one'> cobj = new   
+                <input spellcheck="false" id="two" maxlength="11" style="width: 105px;" v-model="two">{ <p id="comment">//Create
+                  an array of Customer objects with 3 array-objects</p><br>
+                } <br><button class="button-9" id="btn1" @click="validate_one()"> {{ button_value }} </button>
+              </div>
+      
+              <div class="codebox" id="codebox" :style="{ display: display[1] }">
+                <pre>
+import java.util.Scanner;
+import java.util.Date;
+
+public class TestDriver {
+
+    public static void main(String[] args) {
+        Customer[] cobj= new Customer[3];
+                </pre>
+                <br>&emsp;&emsp;&emsp;cobj[0] = 
+                <input spellcheck="false" maxlength="3" style="width: 49px;" v-model='three'> Customer("
+                <input spellcheck="false" maxlength="3" style="width: 50px;" v-model='four'>", "
+                <input spellcheck="false" maxlength="13" style="width: 130px;" v-model='five'>")
+                <p id="comment">//Create a new Customer object using name as "ABC" and email "abc@gmail.com"</p><br>
+                } <br><button class="button-9" id="btn2" @click="validate_two()"> {{ button_value }} </button>
+              </div>
+      
+              <div class="codebox" id="codebox" :style="{ display: display[2] }">
+                <pre>
+import java.util.Scanner;
+import java.util.Date;
+
+public class TestDriver {
+
+    public static void main(String[] args) {
+        Customer[] cobj= new Customer[3];
+        cobj[0]=new Customer("ABC", "abc@gmail.com");
+        cobj[1]=new Customer("DEF", 8654678976L);
+        cobj[2]=new Customer("GHI", "ghi@gmail.com", 1234567890L);
+      
+      </pre>
+          <br>&emsp;&emsp;&emsp;cobj[0].
+                <input spellcheck="false" maxlength="7" style="width: 75px;" v-model='six'>(
+                <input spellcheck="false" maxlength="10" style="width: 110px;" v-model='seven'>);
+                <br>&emsp;
+                <p id="comment">//Set the City of first object to "Coimbatore"</p><br>&emsp;}
+                <br>
+                } <br><button class="button-9" id="btn3" @click="validate_three()"> {{ button_value }} </button>
+              </div>
+      
+
+
+              <div class="codebox" id="codebox" :style="{ display: display[3] }">
+                <pre>
+import java.util.Scanner;
+import java.util.Date;
+
+public class TestDriver {
+
+    public static void main(String[] args) {
+        Customer[] cobj= new Customer[3];
+        cobj[0]=new Customer("ABC", "abc@gmail.com");
+        cobj[1]=new Customer("DEF", 8654678976L);
+        cobj[2]=new Customer("GHI", "ghi@gmail.com", 1234567890L);
+        cobj[0].setCity("Coimbatore");
+        cobj[1].setCity("Coimbatore");
+        cobj[2].setCity("Chennai");
+      </pre>    &emsp;&emsp;&emsp;
+                <input spellcheck="false" maxlength="7" style="width: 75px;" v-model='eight'> vobj = new
+                <input spellcheck="false" maxlength="7" style="width: 75px;" v-model='nine'>(
+                <input spellcheck="false" maxlength="4" style="width: 60px;" v-model='ten'>;)
+                
+                <br>&emsp;}
+                <p id="comment">//Create a new Vehicle object with value "1234"</p><br>
+                } <br><button class="button-9" id="btn4" @click="validate_four()"> {{ button_value }} </button>
+              </div>
+      
+              <div class="codebox" id="codebox" :style="{ display: display[4] }">
+                <pre>public class Person {
+      
+      public String name;
+      
+      public Person(String name){
+          this.name = name;
+      }
+      
+      public String getName(){
+          return name;
+      }
+      </pre>    &emsp;public void
+                <input spellcheck="false" maxlength="7" style="width: 85px;" v-model='eleven'>(
+                <input spellcheck="false" maxlength="6" style="width: 75px;" v-model='twelve'>)
+                <input spellcheck="false" maxlength="4" style="width: 60px;" v-model='thirteen'>){
+                <br>&emsp;&emsp;
+                <input spellcheck="false" maxlength="9" style="width: 100px;" v-model='fourteen'> = name;
+                <br>&emsp;}
+                <p id="comment">//Create a setName method</p><br>
+                } <br><button class="button-9" id="btn5" @click="validate_five()"> {{ button_value }} </button>
+              </div>
+      
+            </div>
+          </div>
+          </main>
+</template>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+  background-color: #F9FAFE;
+}
+
+@font-face {
+  font-family: "Montserrat";
+  src: local("Montserrat"),
+    url(.\fonts\Montserrat-VariableFont_wght.ttf) format("truetype");
+
+  /* add a terminal font here - inconsolata? */
+}
+
+h1,
+h2,
+p {
+  font-family: 'Montserrat', sans-serif;
+  color: rgb(0, 3, 40);
+}
+
+.column {
+  float: left;
+  width: 50%;
+}
+
+/* Clear floats after the columns */
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+mark1 {
+  background-color: rgb(176, 230, 183);
+  box-shadow: 1px 6px 15px 2px rgb(160, 222, 159);
+  color: black;
+  border-radius: 10px;
+  padding-left: 12px;
+  padding-right: 12px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+}
+
+mark0 {
+  box-shadow: 1px 6px 15px 2px rgb(249, 255, 191);
+  background-color: rgb(231, 238, 162);
+  color: black;
+  border-radius: 10px;
+  padding-left: 12px;
+  padding-right: 12px;
+  padding-top: 6px;
+  padding-bottom: 6px;
+}
+
+#comment {
+  margin-left: 15px;
+  font-style: italic;
+  color: gray;
+  font-size: small;
+}
+
+
+.codebox {
+  height: 80%;
+  width: 90%;
+  padding: 25px;
+  border-radius: 10px;
+  background-color: #F9FAFE;
+  box-shadow: 2px 3px 10px 2px #D7DFFF;
+}
+
+input{
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 4px;
+  font-size: 14px;
+  background-color: white;
+  color: black;
+}
+
+/* CSS */
+.button-9 {
+  appearance: button;
+  backface-visibility: hidden;
+  background-color: #405cf5;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .1) 0 2px 5px 0, rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Ubuntu, sans-serif;
+  font-size: 100%;
+  height: 44px;
+  line-height: 1.15;
+  margin: 12px 0 0;
+  outline: none;
+  overflow: hidden;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  transform: translateZ(0);
+  transition: all .2s, box-shadow .08s ease-in;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  width: 200px;
+}
+
+.button-9:disabled {
+  cursor: default;
+}
+
+.button-9:focus {
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
+}
+</style>
--- exp-5/src/views/HomeView.vue
+++ exp-5/src/views/HomeView.vue
@@ -3,13 +3,15 @@ import CustomerView from '../components/CustomerViewComponent.vue'
 import ReservationView from '../components/ReservationViewComponent.vue'
 import VehicleView from '../components/VehicleViewComponent.vue'
 import InstructionsView from '../components/InstructionsViewComponent.vue'
+import RegistrationView from '../components/RegistrationSystemViewComponent.vue'
 
 export default {
   components: {
     CustomerView,
     ReservationView,
     VehicleView,
-    InstructionsView
+    InstructionsView,
+    RegistrationView
   },
   data(){
     return {
@@ -23,6 +25,7 @@ export default {
   <button @click="componentSelect='CustomerView'">Customer View</button>
   <button @click="componentSelect='ReservationView'">Reservation View</button>
   <button @click="componentSelect='VehicleView'">Vehicle View</button>
+  <button @click="componentSelect='RegistrationView'">Registration System View</button>
   <KeepAlive>
     <component v-bind:is="componentSelect" />
   </KeepAlive>
