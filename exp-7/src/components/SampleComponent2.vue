40a7d4ba086be751f6e19f85543cb43fa10a97ab
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-12-26 18:48:10 +0530

    setup router in exp-7

---
+++ exp-7/src/components/SampleComponent2.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+    <h1>Sample Component 2</h1>
+  <button @click="count++">You clicked me {{ count }} times.</button>
+</template>
---
+++ exp-7/src/views/TestView.vue
@@ -0,0 +1,58 @@
+<template>
+
+<div id="app">
+    <div v-html= "validate()"></div>
+    <div class="live-code">
+        <span>
+            class <input type="text"> extends Consumer {
+                <br>
+                <input type="text">() {
+                    <br>
+                    <input type="text">
+                    <br>
+                }
+                <br>
+            }
+        </span>
+    </div>
+</div>
+
+</template>
+
+
+<script>
+
+export default{
+    data: () => {
+        return{
+            code: [`<h1>Test</h1>`,`<h2>Test</h2>`],
+            lineCounter: 0,
+        }
+    },
+
+    methods: {
+        validate(){
+            this.lineCounter++;
+            console.log(this.lineCounter)
+            return this.render(this.lineCounter);
+        },
+
+        render(j){
+            let html = "";
+            for(let i=0;i<=j;i++){
+                if(i>=this.code.length){
+                    break;
+                }
+                html += this.code[i];
+
+            }
+            console.log(html)
+            return html;
+        }
+    }
+  
+}
+
+
+
+</script>
