2f4e2540f0364d5ee7244c138eb5befc80e685ad
Nirmal <nirmalkarthikeyan1503@gmail.com>
2022-12-28 13:12:05 +0530

    verification for exp-3 done

--- exp-3/src/views/ScreenOne.vue
+++ exp-3/src/views/ScreenOne.vue
@@ -3,19 +3,19 @@ import { ref } from 'vue'
 export default {
   setup() {
     const items = ref([
-      {id: 0, word: 'cName', list: 1},
-      { id: 1, word: 'rName', list: 1 },
-      { id: 2, word: 'regNum', list: 1 },
-      { id: 3, word: 'keyStatus', list: 1 },
+      { id: 0, word: 'customerName', list: 1 },
+      { id: 1, word: 'clerkName', list: 1 },
+      { id: 13, word: 'regNum', list: 1 },
+      { id: 14, word: 'keyStatus', list: 1 },
       { id: 4, word: 'customerObj', list: 1 },
       { id: 5, word: 'vehicleObj', list: 1 },
       { id: 6, word: 'contractStatus', list: 1 },
       { id: 7, word: 'releaseDate', list: 1 },
-      { id: 8, word: 'String', list: 13},
-      { id: 9, word: 'Date', list: 13},
-      { id: 10, word: 'Customer', list: 13},
-      {id: 11, word: 'Vehicle', list: 13},
-      {id: 12, word: 'int', list: 13},
+      { id: 8, word: 'String', list: 13 },
+      { id: 9, word: 'Date', list: 13 },
+      { id: 10, word: 'Customer', list: 13 },
+      { id: 11, word: 'Vehicle', list: 13 },
+      { id: 12, word: 'int', list: 13 },
     ])
 
     const getList = (list) => {
@@ -24,13 +24,13 @@ export default {
 
     const startDrag = (event, item) => {
       console.log(item)
-      if(item.list === 13){
+      if (item.list === 13) {
         console.log(13)
         event.dataTransfer.dropEffect = 'copy'
         event.dataTransfer.effectAllowed = 'copy'
       }
 
-      else{
+      else {
         event.dataTransfer.dropEffect = 'move'
         event.dataTransfer.effectAllowed = 'move'
       }
@@ -74,66 +74,87 @@ export default {
       valid: true
     }
   },
-  methods:{
-    validate(){
-      let final_list = []
+  methods: {
+    validate() {
+      let customer = []
+      let vehicle = []
+      let clerk = []
+      let reservation = []
       let i = 0;
       let j = 0;
-      for (i = 6; i < 12; i++){
-        final_list.push(this.getList(i))
+      if (this.getList(5).length != this.getList(6).length) {
+        console.log('The number of variables and datatypes should match.')
+        alert('The number of variables and datatypes should match.')
       }
-      console.log(final_list)
-      for(j = 0; j < final_list.length; j++){
-        let word = ''
-        for (i = 0; i < final_list[j].length;i++){
-          word = word + final_list[j][i].word + ' '
+
+      if (this.getList(7).length != this.getList(8).length) {
+        console.log('The number of variables and datatypes should match.')
+        alert('The number of variables and datatypes should match.')
+      }
+
+      if (this.getList(9).length != this.getList(10).length) {
+        console.log('The number of variables and datatypes should match.')
+        alert('The number of variables and datatypes should match.')
+      }
+
+      if (this.getList(11).length != this.getList(12).length) {
+        console.log('The number of variables and datatypes should match.')
+        alert('The number of variables and datatypes should match.')
+      }
+
+      else {
+        for (i = 0; i < this.getList(5).length; i++) {
+          customer.push(this.getList(5)[i].word + ' ' + this.getList(6)[i].word)
         }
-        final_list[j] = word
+
+        for (i = 0; i < this.getList(7).length; i++) {
+          clerk.push(this.getList(7)[i].word + ' ' + this.getList(8)[i].word)
+        }
+
+        for (i = 0; i < this.getList(9).length; i++) {
+          vehicle.push(this.getList(9)[i].word + ' ' + this.getList(10)[i].word)
+        }
+
+        for (i = 0; i < this.getList(11).length; i++) {
+          reservation.push(this.getList(11)[i].word + ' ' + this.getList(12)[i].word)
+        }
+
+        console.log(customer)
+        console.log(clerk)
+        console.log(vehicle)
+        console.log(reservation)
       }
 
-      let final_string = final_list.join(' ')
+      if(!customer.includes('customerName String'))
+        alert("There's a type mismatch in the Customer class.")
 
-      if(!final_string.includes('locates the vehicle')){
-        this.valid = false
-        console.log('Locate the vehicle not found')
-      }
+    else if(!clerk.includes('clerkName String'))
+        alert("There's a type mismatch in the Clerk class.")
 
-      if(!final_string.includes('marks the contract active')){
-        this.valid = false
-        console.log('Marks the contract active not found')
-      }
+    else if(!vehicle.includes('regNum int'))
+        alert("There's a type mismatch in the Vehicle class.")
 
-      if(!final_string.includes('acquire a vehicle')){
-        this.valid = false
-        console.log('Acquire a vehicle not found')
-      }
+    else if(!vehicle.includes('keyStatus String'))
+        alert("There's a type mismatch in the Vehicle class.")
 
+    else if(!vehicle.includes('customerObj Customer'))
+        alert("There's a type mismatch in the Vehicle class.")
 
-      if(!final_string.includes('signs the contract')){
-        this.valid = false
-        console.log('Signs the contract not found')
-      }
+    else if(!vehicle.includes('vehicleObj Vehicle'))
+        alert("There's a type mismatch in the Vehicle class.")
 
-      if(!final_string.includes('gives the key')){
-        this.valid = false
-        console.log('Gives the keys not found')
-      }
+    else if(!reservation.includes('contractStatus String'))
+        alert("There's a type mismatch in the Reservation class.")
 
-      if(!final_string.includes('vehicle type is not available')){
-        this.valid = false
-        console.log('Vehicle type is not available name not found')
-      }
+    else if(!reservation.includes('releaseDate Date'))
+        alert("There's a type mismatch in the Reservation class.")
 
-     
-      if(this.valid){
-        alert('Correct')
-        this.$router.push('/screen-three')
-      }
-      else{
-        console.log('There\'s some mistake')
-      }
+    else{
+      alert('Success')
+      this.$router.push('/screen-two')
+    }
+        
 
-      console.log(final_string)
     }
   }
 }
@@ -141,141 +162,140 @@ export default {
 
 
 <template class="app">
-    <header style="padding: 20px">
+  <header style="padding: 20px">
     <h1>Step 1: Associating functions and return types</h1>
     <p>Map the elements and respective datatypes to their classes by dragging and dropping.</p>
   </header>
-    <main>
-        <div class="flexbox">
-            <div class="flex-between">
-              <h3>Variables</h3>
-    <div class="identify-box drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(1)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
+  <main>
+    <div class="flexbox">
+      <div class="flex-between">
+        <h3>Variables</h3>
+        <div class="identify-box drop-zone" @drop="onDrop($event, 1)" @dragenter.prevent @dragover.prevent>
+          <div v-for="item in getList(1)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+            @dragstart="startDrag($event, item)">
+            <p>{{ item.word }}</p>
+          </div>
         </div>
-    </div>
-  </div>
+      </div>
 
-    <div class="flex-between">
+      <div class="flex-between">
         <h3>Customer</h3>
         <div class="flex-horizontal">
           <div class="outer-box">
             <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 5)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(5)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-        </div>
-      </div>
-      <div class="outer-box">
+            <div class="identify-box drop-zone" @drop="onDrop($event, 5)" @dragenter.prevent @dragover.prevent>
+              <div v-for="item in getList(5)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                @dragstart="startDrag($event, item)">
+                <p>{{ item.word }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="outer-box">
             <p>Datatype</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 6)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(6)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
+            <div class="identify-box drop-zone" @drop="onDrop($event, 6)" @dragenter.prevent @dragover.prevent>
+              <div v-for="item in getList(6)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                @dragstart="startDrag($event, item)">
+                <p>{{ item.word }}</p>
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    </div>
-    </div>
-    <div style="height: 50px;"></div>
-    <div>
-    <h3>Clerk</h3>
-    <div class="flex-horizontal">
-      <div class="outer-box">
-            <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 7)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(7)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
+        <div style="height: 50px;"></div>
+        <div>
+          <h3>Clerk</h3>
+          <div class="flex-horizontal">
+            <div class="outer-box">
+              <p>Variable</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 7)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(7)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+            <div class="outer-box">
+              <p>Datatype</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 8)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(8)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+        <div style="height: 50px;"></div>
+        <div>
+          <h3>Vehicle</h3>
+          <div class="flex-horizontal">
+            <div class="outer-box">
+              <p>Variable</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 9)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(9)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+            <div class="outer-box">
+              <p>Variable</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 10)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(10)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-        <div class="outer-box">
-            <p>Datatype</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 8)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(8)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-    </div>
-        </div>
-    </div>
-</div>
-<div style="height: 50px;"></div>
-<div>
-    <h3>Vehicle</h3>
-    <div class="flex-horizontal">
-      <div class="outer-box">
-            <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 9)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(9)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-        </div>
-      </div>
-      <div class="outer-box">
-            <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 10)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(10)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-    </div>
-      </div>
-    </div>
-</div>
-<div style="height: 50px;"></div>
-<div>
-    <h3>Reservation</h3>
- 
-    <div class="flex-horizontal">
-      <div class="outer-box">
-            <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 11)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(11)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-        </div>
-      </div>
-      <div class="outer-box">
-            <p>Variable</p>
-        <div class="identify-box drop-zone" @drop="onDrop($event, 12)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(12)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
-        </div>
-    </div>
-      </div>
-    </div>
-</div>
+        <div style="height: 50px;"></div>
+        <div>
+          <h3>Reservation</h3>
 
-    </div>
-    <div class="flex-between">
-        <h3>Variables</h3>
-    <div class="identify-box drop-zone" @drop="onDrop($event, 13)" @dragenter.prevent @dragover.prevent>
-      <div v-for="item in getList(13)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
-        @dragstart="startDrag($event, item)">
-        <p>{{ item.word }}</p>
+          <div class="flex-horizontal">
+            <div class="outer-box">
+              <p>Variable</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 11)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(11)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+            <div class="outer-box">
+              <p>Variable</p>
+              <div class="identify-box drop-zone" @drop="onDrop($event, 12)" @dragenter.prevent @dragover.prevent>
+                <div v-for="item in getList(12)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+                  @dragstart="startDrag($event, item)">
+                  <p>{{ item.word }}</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
+      </div>
+      <div class="flex-between">
+        <h3>Datatypes</h3>
+        <div class="identify-box drop-zone" @drop="onDrop($event, 13)" @dragenter.prevent @dragover.prevent>
+          <div v-for="item in getList(13)" :key="item.id" class="to-identify drag-el-horizontal" draggable="true"
+            @dragstart="startDrag($event, item)">
+            <p>{{ item.word }}</p>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
-  </div>
-  <footer>
-    <div id="buttons" class="relative">
-      <button class="navitem">Back</button>
-      <button class="navitem" @click="validate()">Next</button>
-    </div>
-  </footer>
-    </main>
+    <footer>
+      <div id="buttons" class="relative">
+        <button class="navitem">Back</button>
+        <button class="navitem" @click="validate()">Next</button>
+      </div>
+    </footer>
+  </main>
 </template>
 
 <style scoped>
-
-.image{
+.image {
   align-items: center;
   display: flex;
   flex-direction: row;
@@ -285,37 +305,38 @@ export default {
   padding: 0;
 }
 
-.identify-box{
+.identify-box {
   background-color: #fff;
   box-shadow: 2px 3px 10px 2px #D7DFFF;
 }
 
-.to-identify{
+.to-identify {
   background-color: #F9FAFE;
   box-shadow: 2px 3px 10px 2px #D7DFFF;
- 
+
 }
-.identified{
+
+.identified {
   background-color: #6D71FA;
   color: white;
 }
 
-h3{
+h3 {
   margin-bottom: 0px;
 }
 
-.unidentified{
+.unidentified {
   background-color: #fff;
   color: black;
 }
 
-.flex-between{
-    display: flex;
-    flex-direction: column;
-    margin-left: 10px;
-    margin-right: 10px;
-    width: auto;
-    gap: 10px;
+.flex-between {
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  margin-right: 10px;
+  width: auto;
+  gap: 10px;
 }
 
 .drop-zone {
@@ -334,23 +355,23 @@ h3{
   padding: 5px;
 }
 
-.outer-box{
+.outer-box {
   padding: 5px;
-    background-color: #F9FAFE;
-    /* box-shadow: 1px 2px 5px 1px #D7DFFF; */
-    max-width: 600px;
-    min-width: fit-content;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+  background-color: #F9FAFE;
+  /* box-shadow: 1px 2px 5px 1px #D7DFFF; */
+  max-width: 600px;
+  min-width: fit-content;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 }
 
-.flex-horizontal{
+.flex-horizontal {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
-  justify-content:flex-start;
+  justify-content: flex-start;
 }
 
 .drag-el-horizontal {
@@ -382,11 +403,11 @@ h3{
   padding: 10px;
 }
 
-.drag-el-horizontal:nth-last-child(){
+.drag-el-horizontal:nth-last-child() {
   margin-bottom: 0;
 }
 
-.drag-el:nth-last-child(){
+.drag-el:nth-last-child() {
   margin-bottom: 0;
 }
 
@@ -409,14 +430,15 @@ h3{
 }
 
 .relative {
- padding: 10px;
- display: flex;
- width: 100%;
- flex-direction: row;
- justify-content: space-between;
-} 
+  padding: 10px;
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
 .navitem {
-  width: 100px; 
+  width: 100px;
   height: 50px;
   padding: 10px;
   text-align: center;
@@ -434,6 +456,5 @@ h3{
   cursor: pointer;
   font-weight: bold;
 }
-
 </style>
 
