27c3dc14d5ee1f296ed77605f6cc46a3d3cd86ab
nitinravi <nitinravi18@gmail.com>
2022-12-29 14:54:23 +0530

    added images

--- exp-2/src/views/PageThree.vue
+++ exp-2/src/views/PageThree.vue
@@ -1,6 +1,11 @@
 <template>
     <main>
-        <img src="..\..\assets\time_sequence.png"/>
+        <div class="codebox">
+            <img style= "max-width: 800px;" src="..\..\assets\Frame 1.png"/>
+        </div>
+        <div class="codebox" style="max-width: 300px;">
+        <!--Create an unordered -->
+        </div>
     </main>
 </template>
 
@@ -11,4 +16,11 @@ export default{
 </script>
 
 <style scoped>
+.codebox {
+  max-width: 800px;
+  padding: 25px;
+  border-radius: 10px;
+  background-color: #F9FAFE;
+  box-shadow: 2px 3px 10px 2px #D7DFFF;
+}
 </style>
