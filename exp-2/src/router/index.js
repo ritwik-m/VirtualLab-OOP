b2f24f1f52631393bdd237ce83ecbb7635e676ea
sooryasrajan <soorya.s27@gmail.com>
2023-01-04 19:50:56 +0530

    Added completion page in second exp

--- exp-2/src/router/index.js
+++ exp-2/src/router/index.js
@@ -1,34 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import PageOne from '../views/PageOne.vue'
-import PageTwo from '../views/PageTwo.vue'
-import PageThree from '../views/PageThree.vue'
-import PageZero from '../views/PageZero.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import PageOne from "../views/PageOne.vue";
+import PageTwo from "../views/PageTwo.vue";
+import PageThree from "../views/PageThree.vue";
+import PageZero from "../views/PageZero.vue";
+import CongratulationsPage from "../views/CongratulationsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'page-zero',
-      component: PageZero
+      path: "/",
+      name: "page-zero",
+      component: PageZero,
     },
     {
-      path: '/screen-one',
-      name: 'page-one',
-      component: PageOne
+      path: "/screen-one",
+      name: "page-one",
+      component: PageOne,
     },
     {
-      path: '/screen-two',
-      name: 'page-two',
-      component: PageTwo
+      path: "/screen-two",
+      name: "page-two",
+      component: PageTwo,
     },
     {
-      path: '/screen-three',
-      name: 'page-three',
-      component: PageThree
+      path: "/screen-three",
+      name: "page-three",
+      component: PageThree,
     },
-  ]
-})
+    {
+      path: "/completion",
+      name: "Congratulations",
+      component: CongratulationsPage,
+    },
+  ],
+});
 
-export default router
+export default router;
